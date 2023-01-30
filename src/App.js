c614220d6aab454ee9a3eafdfc492d227a329778
SHyeonnnnn <dnsdkatmdgus@naver.com>
2023-01-30 13:16:27 +0900

    movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }//1
+
+  componentDidMount(){//3
+    setTimeout(() => {
+      this.setState({isLoading:false})//isLoading을 false로 만드는데 5초걸려라
+    }, 5000);
+  }
+  render() {//2
+    const {isLoading} = this.state;
+    return (
+      <div>
+        {isLoading ? 'Loading...' : 'We are ready'}
+      </div>
+    );
+  }
 }
 
 export default App;
